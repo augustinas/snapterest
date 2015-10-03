1c9057ecb1580935b84ada22f1fd1b9515252812
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-10-03 15:19:46 +0100

    Adds StreamTweet mounting methods

---
+++ source/components/StreamTweet.react.js
@@ -0,0 +1,53 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var Header = require("./Header.react");
+var Tweet = require("./Tweet.react");
+
+var StreamTweet = React.createClass({
+
+  getInitialState: function() {
+    console.log("[Snapterest] StreamTweet: 1. Running getInitialState()");
+
+    return {
+      numberOfCharactersIsInreasing: null,
+      headerText: null
+    };
+  },
+
+  componentWillMount: function() {
+    console.log("[Snapterest] StreamTweet: 2. Running componentWillMount()");
+
+    this.setState({
+      numberOfCharactersIsInreasing: true,
+      headerText: "Latest public photo from Twitter"
+    });
+
+    window.snapterest = {
+      numberOfReceivedTweets: 1,
+      numberOfDisplayedTweets: 1
+    };
+  },
+
+  componentDidMount: function() {
+    console.log("[Snapteres] StreamTweet: 3. Running componentDidMount()");
+
+    var componentDOMRepresentation = ReactDOMNode(this);
+
+    window.snapterest.headerHtml = componentDOMRepresentation.children[0].outerHTML;
+    window.snapterest.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
+  },
+
+  render: function() {
+    console.log("[Snapterest] StreamTweet: Running render()");
+
+    return (
+      <section>
+        <Header text={this.state.headerText} />
+        <Tweet tweet={this.props.tweet}
+               onImageClick={this.props.onAddTweetToCollection} />
+      </section>
+    );
+  }
+});
+
+module.exports = StreamTweet;
